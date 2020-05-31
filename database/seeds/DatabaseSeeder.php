d463dbd441a5874bf397a05dbec566e7857fe662
gomessdaniela <gomess.daniela@gmail.com>
2020-05-30 23:00:05 -0300

    database files added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(CustomersTableSeeder::class);
+        $this->call(CustomersHasGroupClassesTableSeeder::class);
+        $this->call(GroupClassesTableSeeder::class);
+        $this->call(InstructorsTableSeeder::class);
+        $this->call(InstructorsHasClassesTableSeeder::class);
+        $this->call(PaymentsTableSeeder::class);
+        $this->call(PlansTableSeeder::class);
+        $this->call(PhysicalAssessmentsTableSeeder::class);
+        $this->call(UserRolesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(PermissionRoleTableSeeder::class);
+        $this->call(DataRowsTableSeeder::class);
+        $this->call(DataTypesTableSeeder::class);
+        $this->call(FailedJobsTableSeeder::class);
+        $this->call(MigrationsTableSeeder::class);
+        $this->call(SettingsTableSeeder::class);
     }
 }
